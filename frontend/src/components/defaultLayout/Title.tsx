f29bdb77ccd2b1b97da490978c1beae2870a0eb1
yagizerdem <126137803+yagizerdem@users.noreply.github.com>
2025-11-11 17:47:22 +0300

    tile bar gui finshed

--- frontend/src/components/defaultLayout/Title.tsx
+++ frontend/src/components/defaultLayout/Title.tsx
@@ -1,15 +1,39 @@
 import logo from "../../assets/Logo.png";
+import { Button } from "../ui/button";
+import { Fullscreen, Minus, X } from "lucide-react";
 
 export function Title() {
   return (
-    <div className="w-full h-12 bg-(--color-secondary) flex flex-row items-center draggable">
+    <div className="w-full h-12 bg-(--color-secondary) flex flex-row items-center draggable justify-between">
       <div className="flex flex-row items-center">
         <img src={logo} alt="Logo" className="h-8 ml-4 rounded-sm" />
         <h1 className="text-(--color-foreground) ml-4 text-lg font-semibold">
           Museek
         </h1>
       </div>
-      <div className="flex flex-row gap-1"></div>
+      <div className="flex flex-row gap-1 ">
+        <Button
+          size="sm"
+          variant="outline"
+          className="mr-2 cursor-pointer no-drag"
+        >
+          <Minus />
+        </Button>
+        <Button
+          variant="outline"
+          size="sm"
+          className="mr-2 cursor-pointer no-drag"
+        >
+          <Fullscreen />
+        </Button>
+        <Button
+          variant="destructive"
+          size="sm"
+          className="mr-2 cursor-pointer no-drag"
+        >
+          <X />
+        </Button>
+      </div>
     </div>
   );
 }
